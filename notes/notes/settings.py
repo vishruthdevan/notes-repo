64591d96ab3790df54fc86507a3b63d876b95d79
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-26 20:57:52 +0530

    Hide django secret key

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from . import secret
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-y6p-)9h@%c(=l(wu5k)fctzj5$&i!j!)180hj*gq3q_pc+xr'
+SECRET_KEY = secret.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
