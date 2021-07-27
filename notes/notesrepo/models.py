a18e8efdee5d5686ee5d01c9c2cad67cfd665d7e
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-27 19:17:28 +0530

    Configure models required for the project

--- notes/notesrepo/models.py
+++ notes/notesrepo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    topic = models.CharField(max_length=50)
+    note_file = models.FileField(upload_to='notes/%Y/%m/%d/')
+    course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
+class Course(models.Model):
+    code = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    exp = models.IntegerField()
