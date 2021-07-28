e1cc12d17bda202630fd18b1b5d7a4932db32f17
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-28 21:57:44 +0530

    Add views to views.py

--- notes/notesrepo/urls.py
+++ notes/notesrepo/urls.py
@@ -1,8 +1,12 @@
+from django.http import request
 from django.urls import path
 from django.urls.conf import include
+from . import views
+
 
 urlpatterns = [
-    path('courses/',),
-    path('courses/<slug:code>',),
-    path('courses/<slug:code>/notes',),
+    path('', views.index),
+    path('courses/', views.CourseListView.as_view()),
+    # path('courses/<slug:code>',),
+    # path('courses/<slug:code>/notes',),
 ]
--- notes/notesrepo/views.py
+++ notes/notesrepo/views.py
@@ -1,3 +1,13 @@
+from django.http import response
 from django.shortcuts import render
+from django.views import generic
+from django.views import View
+from .models import Course
+
+def index(request):
+    return render(request, 'notesrepo/index.html')
+
 
 # Create your views here.
+class CourseListView(generic.ListView):
+    model = Course
