c6a3cf48db4073cf8edae81d1d887b6278f7dbb9
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-28 21:57:06 +0530

    Configure models and register to admin

--- notes/notesrepo/admin.py
+++ notes/notesrepo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Course, Note, Author
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Note)
+admin.site.register(Author)
