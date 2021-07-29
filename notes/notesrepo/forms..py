368a77046ff403e3d5f3df56d9559c1be8b03dac
Vishruth Devan <vishruthdevan234@gmail.com>
2021-07-29 22:34:06 +0530

    Add form object to enter course

---
+++ notes/notesrepo/forms..py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import fields
+from .models import *
+
+class CourseForm(forms.ModelForm):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
