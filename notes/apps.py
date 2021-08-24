a09b91c80e31cbf538d30ca9772a2493524d7c23
Vishruth Devan <vishruthdevan234@gmail.com>
2021-08-24 21:05:20 +0530

    Change project and app names cause it makes more sense

--- notes/apps.py
+++ notes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NotesrepoConfig(AppConfig):
+class NotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notesrepo'
+    name = 'notes'
---
+++ notesrepo/secret.py
@@ -0,0 +1,1 @@
+secret_key = 'django-insecure-d-y6p-)9h@%c(=l(wu5k)fctzj5$&i!j!)180hj*gq3q_pc+xr'
