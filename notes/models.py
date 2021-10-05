170fce4b4d5fd23a2199571a91e96138722d00f5
Vishruth Devan <vishruthdevan@gmail.com>
2021-10-05 21:23:24 +0530

    Shift author model to a seperate app

--- notes/models.py
+++ notes/models.py
@@ -3,16 +3,6 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    exp = models.IntegerField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.name
-
 class Note(models.Model):
     topic = models.CharField(max_length=50)
     note_file = models.FileField(upload_to='notes/%Y/%m/%d/')
