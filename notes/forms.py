f3f50622d76bcc6b1aaeaf6d023f5c30d9d36667
Vishruth <vishruthdevan@gmail.com>
2022-01-20 08:48:26 +0530

    Move signup view to users app

--- notes/forms.py
+++ notes/forms.py
@@ -5,26 +5,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.contrib.auth.models import User
 
-class CourseForm(forms.ModelForm):    
+
+class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = '__all__'
 
-class RegisterForm(UserCreationForm):
-    name = forms.CharField(max_length=50)
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = UserCreationForm.Meta.fields + ('name',)
-
-class AuthorForm(forms.ModelForm):
-    class Meta:
-        model = Author
-        fields = ['name', 'exp']
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
         labels = {
-            'text' : 'Add a comment'
+            'text': 'Add a comment'
         }
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import *
+from django.urls import reverse
+from . import forms
+from django.contrib.auth import login
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class Signup(View):
+    def get(self, request):
+        form = forms.RegisterForm()
+        return render(request, "registration/signup.html", {'form': form})
+
+    def post(self, request):
+        context = request.POST.copy()
+        context['exp'] = 0
+        form = forms.RegisterForm(request.POST)
+        aform = forms.AuthorForm(context)
+        if form.is_valid() and aform.is_valid():
+            form.save()
+            user = User.objects.get(username=request.POST['username'])
+            instance = aform.save(commit=False)
+            instance.user = user
+            instance.save()
+            login(self.request, user)
+            return redirect(reverse('index'))
+        else:
+            return render(self.request, 'registration/signup.html', {'form': form})
