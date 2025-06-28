f523345f4f9dd95378246e76296dcf1cd9235a54
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:55:48 -0400

    Implement user login logout

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -117,6 +117,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
