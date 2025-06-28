e15833b39b9641d5d7273b0d8eb642065d6ba059
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:55:48 -0400

    Add urlpaths to urls.py

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(''/ include('notesrepo.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
