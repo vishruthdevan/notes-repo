2921ce143b9a044479e184dee1755a398d219cd5
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:55:49 -0400

    Add comment create view and url

--- notes/notesrepo/urls.py
+++ notes/notesrepo/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path('courses/<slug:code>/upload-note/', views.NoteCreate.as_view(), name = 'note_create'),
     path('courses/<slug:code>/<int:pk>/delete-note/', views.NoteDelete.as_view(), name = 'note_delete'),
     path('courses/<slug:code>/<int:pk>/update-note/', views.NoteUpdate.as_view(), name = 'note_update'),
-    path('courses/<slug:code>/like/<int:pk>/', views.NoteLike.as_view(), name = 'note_like'),
-    path('courses/<slug:code>/dislike/<int:pk>/', views.NoteDislike.as_view(), name = 'note_dislike')
-    # path('courses/<slug:code>',),
+    path('courses/<slug:code>/<int:pk>/like/', views.NoteLike.as_view(), name = 'note_like'),
+    path('courses/<slug:code>/<int:pk>/dislike/', views.NoteDislike.as_view(), name = 'note_dislike'),
+    path('courses/<slug:code>/<int:pk>/comment',views.CommentCreate.as_view(), name = 'comment_create'),
     # path('courses/<slug:code>/notes',),
 ]
