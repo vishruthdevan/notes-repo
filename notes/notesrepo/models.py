fb425be01b2116b92e59ff39bf6f02b4c49486a3
Vishruth Devan <vishruthdevan@gmail.com>
2025-06-28 11:55:49 -0400

    Add comment model for comments for each note file

--- notes/notesrepo/models.py
+++ notes/notesrepo/models.py
@@ -2,11 +2,21 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    exp = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
 class Note(models.Model):
     topic = models.CharField(max_length=50)
     note_file = models.FileField(upload_to='notes/%Y/%m/%d/')
     course = models.ForeignKey('Course', on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    comments = models.ManyToManyField(Author, through='Comment', related_name='author_comments')
 
     def __str__(self):
         return str(self.topic)
@@ -18,10 +28,11 @@ class Course(models.Model):
     def __str__(self):
         return str(self.code) + ' - ' + str(self.name)
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    exp = models.IntegerField()
+class Comment(models.Model):
+    comment = models.TextField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        if len(self.comment) < 15 : return self.text
+        return self.comment[:11] + ' ...'
